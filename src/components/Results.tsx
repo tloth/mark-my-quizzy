55b7f82e4d154c2beca07ebb292eb9b9936f15f0
Joko Sanyang <joko.sanyang@ticketmaster.co.uk>
2020-04-04 17:01:23 +0100

    add airtable results handler function

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-export default function Marking() {
-  return <p>On the results page</p>
+export default function Results() {
+  const [results, setResults] = useState(null);
+
+  useEffect(() => {
+    fetch('/.netlify/functions/airtableAllResults')
+      .then(response => response.json())
+      .then(data => {
+        console.log({ data });
+        setResults(data);
+      });
+  }, []);
+
+  console.log({ results });
+
+  return results ? <p>results loaded</p> : <p>waiting for results</p>;
 }
